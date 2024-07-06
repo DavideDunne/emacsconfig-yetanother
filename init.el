06ce4dc83f87dbea9d512165b958d7c7b7328854
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2024-07-05 23:09:14 -0600

    add font sizes and org-agenda-files

--- init.el
+++ init.el
@@ -140,3 +140,15 @@
 (global-set-key (kbd "C-c j n") 'org-journal-new-entry)
 
 (setq org-display-inline-images t)
+
+;; Set different font sizes for the org-mode bullets
+(custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+)
+
+;; Where should org-agenda be getting it's schedules from
+(setq org-agenda-files (directory-files-recursively "~/.emacs.d/org-mode" "\\.org$"))
